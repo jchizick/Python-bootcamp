8f92728d8e47fcddea3c00d49384ab35819d870f
Jordan <jordanlanechizick@gmail.com>
2020-09-17 18:37:18 -0400

    Add files via upload

---
+++ fcc-mean-var-std/mean_var_std.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def calculate(list):
+  if len.list != 9:
+    return ValueError"List must contain nine numbers."
+  
+  np.array
+  dictionary = {}
+  
+  
+
+
+
+
+    return calculations
