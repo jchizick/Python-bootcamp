8b7fb2da7fc1cb38d5c1fa14063c96c18a60d7a1
Jordan Chizick <jordanlanechizick@gmail.com>
2020-09-18 00:02:56 -0400

    Ok

--- Exercise_6_Mean_Var_Std/mean_var_std.py
+++ Exercise_6_Mean_Var_Std/mean_var_std.py
@@ -5,11 +5,11 @@ def calculate(list):
     raise ValueError("List must contain nine numbers.")
   x = np.array(list).reshape((3,3))
   calculations = {
-  "mean": [x.mean(0).tolist(), x.mean(1).tolist(), x.mean().tolist()], 
-  "variance": [x.var(0).tolist(), x.var(1).tolist(), x.var().tolist()],
-  "standard deviation": [x.std(0).tolist(), x.std(1).tolist(), x.std().tolist()],  
-  "max": [x.max(0).tolist(), x.max(1).tolist(), x.max().tolist()], 
-  "min": [x.min(0).tolist(), x.min(1).tolist(), x.min().tolist()],  
-  "sum": [x.sum(0).tolist(), x.sum(1).tolist(), x.sum().tolist()] 
+  "mean": [x.mean(0).tolist(), x.mean(1).tolist(), x.mean()], 
+  "variance": [x.var(0).tolist(), x.var(1).tolist(), x.var()],
+  "standard deviation": [x.std(0).tolist(), x.std(1).tolist(), x.std()],  
+  "max": [x.max(0).tolist(), x.max(1).tolist(), x.max()], 
+  "min": [x.min(0).tolist(), x.min(1).tolist(), x.min()],  
+  "sum": [x.sum(0).tolist(), x.sum(1).tolist(), x.sum()] 
   }
   return calculations
