9b81fe8c655e65709ab684a31270b225fc4d7b22
Jordan Chizick <jordanlanechizick@gmail.com>
2020-09-17 23:41:47 -0400

    commit

--- Exercise_6_Mean_Var_Std/mean_var_std.py
+++ Exercise_6_Mean_Var_Std/mean_var_std.py
@@ -1,5 +1,15 @@
 import numpy as np
 
 def calculate(list):
-  
+  if len(list)!= 9:
+    raise ValueError("List must contain nine numbers.")
+  x = np.array(list).reshape((3,3))
+  calculations = {
+  "mean": [x.mean(0).tolist(), x.mean(1).tolist(), x.mean().tolist()], 
+  "variance": [x.var(0).tolist(), x.var(1).tolist(), x.var().tolist()],
+  "standard deviation": [x.std(0).tolist(), x.std(1).tolist(), x.std().tolist()],  
+  "max": [x.max(0).tolist(), x.max(1).tolist(), x.max().tolist()], 
+  "min": [x.min(0).tolist(), x.min(1).tolist(), x.min().tolist()],  
+  "sum": [x.sum(0).tolist(), x.sum(1).tolist(), x.sum().tolist()] 
+  }
   return calculations
